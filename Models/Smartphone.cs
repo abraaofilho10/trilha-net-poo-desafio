8efabff37497ccefa8f80cee45686c9629a481cb
Turritopsis22 <acsf09@gmail.com>
2023-01-26 15:22:12 -0300

    Desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string modelo;
+        private string imei;
+        private int memoria;
+        // Implementado!!!
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // Implementado!!!
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!!!
+Console.WriteLine("Testes do Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "91234-5678", modelo: "Modelo 1", imei: "1111111111", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+Console.WriteLine("Número do Nokia: " + nokia.Numero);
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testes do Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "98765-4321", modelo: "Modelo 2", imei: "222222222", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+Console.WriteLine("Número do iPhone: " + iphone.Numero);
+iphone.InstalarAplicativo("Telegram");
